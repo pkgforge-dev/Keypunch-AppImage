4e09b914baf3be889c7ef9393b55adb0a8dbe281
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-25 13:50:56 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/dev.bragefuglseth.Keypunch.svg
 export DESKTOP=/usr/share/applications/dev.bragefuglseth.Keypunch.desktop
-export STARTUPWMCLASS=dev.bragefuglseth.Keypunch.desktop # Default to Wayland's wmclass. For X11, GTK_CLASS_FIX will force the wmclass to be the Wayland one.
+export STARTUPWMCLASS=dev.bragefuglseth.Keypunch # Default to Wayland's wmclass. For X11, GTK_CLASS_FIX will force the wmclass to be the Wayland one.
 export GTK_CLASS_FIX=1
 
 # Deploy dependencies
