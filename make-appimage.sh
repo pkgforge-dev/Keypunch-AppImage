f49243cc85184b9cb67f868a9137fecf5bcf340a
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-25 13:36:41 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,18 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q keypunch-git | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/dev.bragefuglseth.Keypunch.svg
+export DESKTOP=/usr/share/applications/dev.bragefuglseth.Keypunch.desktop
+export STARTUPWMCLASS=dev.bragefuglseth.Keypunch.desktop # Default to Wayland's wmclass. For X11, GTK_CLASS_FIX will force the wmclass to be the Wayland one.
+export GTK_CLASS_FIX=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
-
-# Additional changes can be done in between here
+quick-sharun /usr/bin/keypunch
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
