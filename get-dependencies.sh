8a65a7392db3fc6f2d5d96b756cd6bebcc7348a6
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-07 05:19:40 -0400

    nah

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,9 +6,12 @@ ARCH=$(uname -m)
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano mesa-zink ! llvm-libs
+get-debloated-pkgs --add-common --prefer-nano ! llvm-libs
 
 echo "Building Keypunch..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 https://raw.githubusercontent.com/archlinuxcn/repo/refs/heads/master/archlinuxcn/keypunch-git/PKGBUILD -O ./PKGBUILD
 make-aur-package
+
+# no need for this
+pacman -Rdd mesa
