32291f8c1d4f505571ab83f0a798d1eda191a41e
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-03-10 18:56:18 +0100

    Directly install `keypunch-git` from `archlinuxcn` in `make-aur-package`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,5 +10,4 @@ get-debloated-pkgs --add-common --prefer-nano
 
 echo "Building Keypunch..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 https://raw.githubusercontent.com/archlinuxcn/repo/refs/heads/master/archlinuxcn/keypunch-git/PKGBUILD -O ./PKGBUILD
-make-aur-package
+make-aur-package --archlinuxcn keypunch-git
